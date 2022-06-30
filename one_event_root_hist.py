e08b94c6db8d542bf5a1ce780f8ef09a6861133e
Ryan Stuve <108438798+rstuve@users.noreply.github.com>
2022-06-30 12:56:41 -0700

    Update one_event_root_hist.py

--- one_event_root_hist.py
+++ one_event_root_hist.py
@@ -5,8 +5,6 @@ Author: Ryan Stuve
 Moves transverse energy data from root file to array,
 presents it as histogram using matplotlib.pyplot
 
-Required to be in same directory as root file
-
 All lines with FILE DEPENDENT need to be reviewed and edited if used
 with other files besides user.bochen.25650990.OUTPUT._000001.root
 """
@@ -16,7 +14,7 @@ import matplotlib.pyplot as plt
 
 layer = input("Which layer? (enter 1 or 2):\n> ") # choose branch of data, FILE DEPENDENT
 
-f = TFile('user.bochen.25650990.OUTPUT._000001.root') # FILE DEPENDENT
+f = TFile('../data/user.bochen.25650990.OUTPUT._000001.root') # FILE DEPENDENT
 tree = f.Get("SCntuple;"+layer) # FILE DEPENDENT
 
 Et = [] # array to store Et values
