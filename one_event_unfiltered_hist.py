4ff0ef216bf28bb4b554481eb66b6739f1deb182
Ryan Stuve <108438798+rstuve@users.noreply.github.com>
2022-07-06 12:17:45 -0700

    Update and rename one_event_root_hist.py to one_event_unfiltered_hist.py

--- one_event_unfiltered_hist.py
+++ one_event_unfiltered_hist.py
@@ -2,24 +2,22 @@
 June 26, 2022
 Author: Ryan Stuve
 
-Moves ET data for one event from root file to array,
+Moves ET data for all layers in one event from root file to array,
 presents it as histogram using matplotlib.pyplot
-
-
 """
 from ROOT import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 event = 0 # choose event
-cycle = '2' # choose branch of data, FILE DEPENDENT
+cycle = '2' # choose branch of data
 
-f = TFile('../data/user.bochen.25650990.OUTPUT._000001.root') # FILE DEPENDENT
-tree = f.Get("SCntuple;"+layer) # FILE DEPENDENT
+f = TFile('../data/user.bochen.25650990.OUTPUT._000001.root')
+tree = f.Get("SCntuple;"cycle)
 
 Et = [] # array to store Et values
 tree.GetEntry(event)
-EtT = tree.scells_Et # FILE DEPENDENT
+EtT = tree.scells_Et
 for value in EtT:
     Et.append(value)
 
