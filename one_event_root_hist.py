c7009c619354eb98c409b9a87238590202e99761
Ryan Stuve <108438798+rstuve@users.noreply.github.com>
2022-07-06 11:31:58 -0700

    Update one_event_root_hist.py

--- one_event_root_hist.py
+++ one_event_root_hist.py
@@ -2,29 +2,30 @@
 June 26, 2022
 Author: Ryan Stuve
 
-Moves transverse energy data from root file to array,
+Moves ET data for one event from root file to array,
 presents it as histogram using matplotlib.pyplot
 
-All lines with FILE DEPENDENT need to be reviewed and edited if used
-with other files besides user.bochen.25650990.OUTPUT._000001.root
+
 """
 from ROOT import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+event = 0 # choose event
 cycle = '2' # choose branch of data, FILE DEPENDENT
 
 f = TFile('../data/user.bochen.25650990.OUTPUT._000001.root') # FILE DEPENDENT
 tree = f.Get("SCntuple;"+layer) # FILE DEPENDENT
 
 Et = [] # array to store Et values
-tree.GetEntry(0)
+tree.GetEntry(event)
 EtT = tree.scells_Et # FILE DEPENDENT
 for value in EtT:
     Et.append(value)
 
 Et = np.asarray(Et)
-plt.hist(Et, bins=100, range=(1000,5000)) # Range is FILE DEPENDENT
+plt.hist(Et, bins=20, range=(0,1000)) # Range is FILE DEPENDENT
 plt.title("scells_Et_Cycle_"+layer)
 plt.xlabel("Tranverse Energy (Mev)")
+plt.ylabel("Counts")
 plt.show()
